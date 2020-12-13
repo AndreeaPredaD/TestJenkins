ec5f28e6750cf4225dd9aa53f9576c27d180e1b4
PREDA Andreea Daniela <Andreea1.Preda@office.orange.intra>
2020-12-13 14:48:25 +0200

    adding files for the second test case

--- TestSetAge.java
+++ TestSetAge.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -31,11 +32,64 @@ public class TestSetAge {
 			assertEquals("verif set varsta minima",18,e.getAge());
 			
 		} catch (CustomException ex) {
-			// TODO Auto-generated catch block
 			ex.printStackTrace();
 			fail("arunca exceptie pe varsta min acceptata");
 			
 		}
+	}
+	
+
+	@Test
+	public void testBoundary2() {
+		//testare daca limitele sunt definite corect 
+		Employee e;
+		try {
+			e = new Employee("Gigel",20,Department.HR);
+			e.setAge(64);
+			assertEquals("verif set varsta maxima",64,e.getAge());
+			
+		} catch (CustomException ex) {
+			ex.printStackTrace();
+			fail("arunca exceptie pe varsta max acceptata");
+		}
+	}
+	
+	@Test
+	public void testBoundary3() {
+		//testare daca limitele sunt definite corect 
+		Employee s;
+		try {
+			s = new Employee("Gigel",20,Department.IT);
+			s.setAge(17);
+			fail("nu arunca exceptie pe varsta in afara intervalului");
+			
+		} catch (CustomException ex) {
+			//ex.printStackTrace();
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void testBoundary4() {
+		//testare daca limitele sunt definite corect 
+		Employee s;
+		try {
+			s = new Employee("Gigel",20,Department.IT);
+			s.setAge(65);
+			fail("nu arunca exceptie pe varsta in afara intervalului");
+			
+		} catch (CustomException e) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test(expected = CustomException.class) //"assertul"
+	public void testErrors() throws CustomException {
+		
+		//testare situatii care genereaza exceptii
+		
+		Employee s = new Employee("Gigel",20,Department.IT);
+		s.setAge(80);
 	}
 	
 
